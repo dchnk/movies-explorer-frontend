6a6245640ff9b6c548af57bc7cfd13ae622169e0
dchnk <seroxe2@yandex.ru>
2023-08-23 23:35:26 +0700

    feat: about-me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,8 +1,17 @@
+import Photo from "../images/photo.jpeg"
+
 function AboutMe() {
   return (
     <section className='about-me'>
       <h2 className="about-me__heading">Студент</h2>
-      <div className="about-me__heading"></div>
+      <div className="about-me__container">
+        <h3 className="about-me__name">Виталий</h3>
+        <p className="about-me__info">Фронтенд-разработчик, 30 лет</p>
+        <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
+и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+        <a href="https://github.com/dchnk" target="_blank" rel="noreferrer" className="about-me__link">Github</a>
+      </div>
+      <img className="about-me__photo" src={Photo} alt="фото"/>
     </section>
   );
 }
