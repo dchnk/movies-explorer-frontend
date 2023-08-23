3a9a383d2d5babcd2757fba9a56ea3d77c076b88
dchnk <seroxe2@yandex.ru>
2023-08-23 21:44:53 +0700

    feat: AboutMe

---
+++ src/components/AboutMe.js
@@ -0,0 +1,10 @@
+function AboutMe() {
+  return (
+    <section className='about-me'>
+      <h2 className="about-me__heading">Студент</h2>
+      <div className="about-me__heading"></div>
+    </section>
+  );
+}
+
+export default AboutMe;
