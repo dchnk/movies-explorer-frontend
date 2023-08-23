ce3c2e5bbba2ddda09afa5576e7a59e7fe2d099a
dchnk <seroxe2@yandex.ru>
2023-08-23 18:37:21 +0700

    feat: techs

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,21 @@ function Header(props) {
 
   return (
     <header className={location.pathname === "/" ? ("header") : ("header header_type_white-background-color")}>
-      <img className="header__logo" src={Logo} alt="логотип" />
-      {props.loggedIn && location.pathname !== "/" && <nav className="header__navigation">
-        <Link className="header__link header__link_type_films">Фильмы</Link>
-        <Link className="header__link header__link_type_saved-films">Сохраненные фильмы</Link>
-      </nav>}
-      {!props.loggedIn && <div className="header__authorize">
-        <Link className="header__link header__link_type_sign-up">Регистрация</Link>
-        <Link className="header__link header__link_type_sign-in">Войти</Link>
-      </div>}
-      {props.loggedIn && location.pathname !== "/" && <div className="header__profile"> 
-        <Link className="header__link header__link_type_profile">Аккаунт</Link>
-        <span className="header__account-icon"/>
-      </div>}
+      <div className="header__content">
+        <img className="header__logo" src={Logo} alt="логотип" />
+        {props.loggedIn && location.pathname !== "/" && <nav className="header__navigation">
+          <Link className="header__link header__link_type_films">Фильмы</Link>
+          <Link className="header__link header__link_type_saved-films">Сохраненные фильмы</Link>
+        </nav>}
+        {!props.loggedIn && <div className="header__authorize">
+          <Link className="header__link header__link_type_sign-up">Регистрация</Link>
+          <Link className="header__link header__link_type_sign-in">Войти</Link>
+        </div>}
+        {props.loggedIn && location.pathname !== "/" && <div className="header__profile">
+          <Link className="header__link header__link_type_profile">Аккаунт</Link>
+          <span className="header__account-icon" />
+        </div>}
+      </div>
     </header>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import AboutProject from "./AboutProject";
 import Promo from "./Promo";
+import Techs from "./Techs";
 
 function Main() {
   return (
     <main className='main'>
       <Promo/>
       <AboutProject/>
+      <Techs/>
     </main>
   );
 }
