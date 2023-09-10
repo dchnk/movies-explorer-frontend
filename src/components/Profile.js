5f5ff30e94d76efc0f5bdfe7abce917a43f2bcc2
dchnk <seroxe2@yandex.ru>
2023-09-10 14:12:42 +0700

    feat: profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,16 +5,16 @@ function Profile() {
         <h1 className="profile__heading">Привет, Виталий!</h1>
         <div className="profile__info">
           <div className="profile__container profile__container_type_name">
-            <p className="profile__name-placaholder">Имя</p>
-            <p className="profile__name">Виталий</p>
+            <p className="profile__placaholder">Имя</p>
+            <p className="profile__content">Виталий</p>
           </div>
           <div className="profile__container">
-            <p className="profile__email-placaholder">E-mail</p>
-            <p className="profile__email">pochta@yandex.ru</p>
+            <p className="profile__placaholder">E-mail</p>
+            <p className="profile__content">pochta@yandex.ru</p>
           </div>
         </div>
         <button className="profile__button">Редактировать</button>
-        <button className="profile__button">Выйти из аккаунта</button>
+        <button className="profile__button profile__button_type_exit">Выйти из аккаунта</button>
       </section>
     </main>
   );
