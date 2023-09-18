f8f7b2129fdb613b124bd5a4a81f315d516d4449
dchnk <seroxe2@yandex.ru>
2023-09-18 12:23:51 +0700

    fix: checkbox

--- src/components/FilterCheckbox.js
+++ src/components/FilterCheckbox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function FilterCheckbox() {
 
-  const [checked, setChecked] = React.useState(false);
+  const [checked, setChecked] = React.useState(true);
   const handleChange = () => {
 
     setChecked(!checked);
@@ -11,13 +11,14 @@ function FilterCheckbox() {
 
   return (
     <div className='checkbox'>
-      <div className='checkbox__content'>
-      <label className={checked ? ("checkbox__switch checkbox__switch_type_active") : ("checkbox__switch")}>
-        <input className="checkbox__input" name="checkbox" id="checkbox" type="checkbox" onClick={handleChange}></input>
-      </label>
-      <p className="checkbox__text">Короткометражки</p>
-      </div>
+    <div className='checkbox__content'>
+    <label className={checked ? ("checkbox__switch checkbox__switch_type_active") : ("checkbox__switch")}>
+      <div className={checked ? ("checkbox__toggle checkbox__toggle_type_active") : ("checkbox__toggle")}></div>
+      <input className="checkbox__input" name="checkbox" id="checkbox" type="checkbox" onClick={handleChange}></input>
+    </label>
+    <p className="checkbox__text">Короткометражки</p>
     </div>
+  </div>
   );
 }
 
