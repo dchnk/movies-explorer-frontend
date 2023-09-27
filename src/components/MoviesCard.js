ba6c709d4ae7e1ed981c1b89738097b0f4744778
dchnk <seroxe2@yandex.ru>
2023-09-27 23:29:09 +0700

    feat: movieCard duration, trailer-link

--- src/components/MoviesCard.js
+++ src/components/MoviesCard.js
@@ -5,11 +5,24 @@ function MoviesCard(props) {
 
   const { movie } = props;
   const location = useLocation();
-
   const [isLiked, setIsLiked] = React.useState(false);
+  const [duration, setDuration] = React.useState('');
+
+  React.useEffect(() => {
+    if (movie.duration === 60) {
+      setDuration('1ч');
+    } else if ((movie.duration > 60) && ((movie.duration % 60) === 0)) {
+      setDuration(`${Math.floor(movie.duration / 60)}ч`);
+    } else if (movie.duration > 60) {
+      setDuration(`${Math.floor(movie.duration / 60)}ч ${movie.duration % 60}м`);
+    } else {
+      setDuration(`${movie.duration % 60}м`);
+    }
+  }, [])
 
   function handleLikeClick() {
     setIsLiked(!isLiked);
+    console.log(movie.duration)
   }
 
   function handleDisikeClick() {
@@ -18,13 +31,13 @@ function MoviesCard(props) {
 
   return (
     <li className="movie">
-      <img className="movie__image" alt="картинка фильма" src={`https://api.nomoreparties.co${movie.image.url}`} />
+      <a href={movie.trailerLink} target="_blank" rel="noreferrer"><img className="movie__image" alt="картинка фильма" src={`https://api.nomoreparties.co${movie.image.url}`} /></a>
       <div className="movie__container">
         <p className="movie__name">{movie.nameRU}</p>
         {location.pathname === "/movies" && <button className={isLiked ? ("movie__like movie__like_active") : ("movie__like")} onClick={handleLikeClick} />}
         {location.pathname === "/saved-movies" && <button className="movie__dislike" onClick={handleDisikeClick} />}
       </div>
-      <p className="movie__duration">1ч 42м</p>
+      <p className="movie__duration">{duration}</p>
     </li>
   );
 }
