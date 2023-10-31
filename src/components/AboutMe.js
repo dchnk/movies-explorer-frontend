cc486a021aa053d13f9b691400c39ed564451298
dchnk <seroxe2@yandex.ru>
2023-10-31 17:03:50 +0700

    about-me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,6 +1,6 @@
 import Photo from "../images/photo.jpeg"
 
-function AboutMe() {  
+function AboutMe() {
 
   return (
     <section className='about-me'>
@@ -8,9 +8,8 @@ function AboutMe() {
         <h2 className="about-me__heading">Студент</h2>
         <div className="about-me__container">
           <h3 className="about-me__name">Виталий</h3>
-          <p className="about-me__info">Фронтенд-разработчик, 30 лет</p>
-          <p className="about-me__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-            и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+          <p className="about-me__info">Фронтенд-разработчик, {new Date().getFullYear() - 1997} лет</p>
+          <p className="about-me__text">Я родился и живу в Новосибирске, учился в СГУВТ на факультете судовождения. У меня есть жена и дочь. Регулярно беру новые интересные и более сложные для себя задачи, чтобы научиться использовать их в работе. Читаю код других разработчиков, беру для себя хорошие практики и улучаю собственный код. Ежедневно практикую английский язык, читаю доку из оригинальных источников. Умею искать ответы самостоятельно, но и не боюсь задавать вопросы более опытным товарищам.</p>
           <a href="https://github.com/dchnk" target="_blank" rel="noreferrer" className="about-me__link">Github</a>
         </div>
         <img className="about-me__photo" src={Photo} alt="фото" />
