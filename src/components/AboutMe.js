86a97d040f6a601e52351af69313196c3c6f7148
dchnk <seroxe2@yandex.ru>
2023-10-31 17:11:03 +0700

    about-me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
       <div className="about-me__content">
         <h2 className="about-me__heading">Студент</h2>
         <div className="about-me__container">
-          <h3 className="about-me__name">Виталий</h3>
+          <h3 className="about-me__name">Денис Дьяченко</h3>
           <p className="about-me__info">Фронтенд-разработчик, {new Date().getFullYear() - 1997} лет</p>
           <p className="about-me__text">Я родился и живу в Новосибирске, учился в СГУВТ на факультете судовождения. У меня есть жена и дочь. Регулярно беру новые интересные и более сложные для себя задачи, чтобы научиться использовать их в работе. Читаю код других разработчиков, беру для себя хорошие практики и улучаю собственный код. Ежедневно практикую английский язык, читаю доку из оригинальных источников. Умею искать ответы самостоятельно, но и не боюсь задавать вопросы более опытным товарищам.</p>
           <a href="https://github.com/dchnk" target="_blank" rel="noreferrer" className="about-me__link">Github</a>
